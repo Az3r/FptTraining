ad401945831a7fe18fb46e12b2f4aac9fa9f0c9d
azer <azerprojects@gmail.com>
2021-09-15 14:20:40 +0700

    AspNet: add Unit of Work, ProductRepository

---
+++ AspNet/Repositories/ProductRepository.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ProductServer.Models;
+
+namespace ProductServer.Repositories
+{
+    public class ProductRepository : IDisposable
+    {
+        public ProductRepository(ProductContext context)
+        {
+            this.context = context;
+        }
+
+        public List<Product> FindProduct(ProductQuery query, int size = 20, int offset = 0)
+        {
+            if (query.IsEmpty())
+            {
+                return context.Products.Skip(offset * size).Take(size).ToList();
+            }
+            List<Product> products = context.Products.Where(p => p.Name.Equals(query.Name, StringComparison.InvariantCultureIgnoreCase)).ToList();
+            return products;
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+
+        private ProductContext context;
+    }
+
+    public class ProductQuery
+    {
+        public string Name { get; set; } = "";
+        public string Category { get; set; } = "";
+        public double MinPrice { get; set; } = double.MinValue;
+        public double MaxPrice { get; set; } = double.MaxValue;
+
+        public bool IsEmpty()
+        {
+            return (MaxPrice == double.MaxValue &&
+                    MinPrice == double.MinValue &&
+                    String.IsNullOrWhiteSpace(Name) &&
+                    String.IsNullOrWhiteSpace(Category));
+        }
+    }
+}
---
+++ AspNet/Repositories/UnitOfWork.cs
@@ -0,0 +1,30 @@
+using System;
+using ProductServer.Models;
+
+namespace ProductServer.Repositories
+{
+    public class UnitOfWork : IDisposable
+    {
+        public UnitOfWork(ProductContext product)
+        {
+            productContext = product;
+            ProductRepository = new ProductRepository(product);
+
+        }
+
+        public void Dispose()
+        {
+            productContext.Dispose();
+        }
+
+        public void Save()
+        {
+            productContext.SaveChanges();
+        }
+
+        public readonly ProductRepository ProductRepository;
+
+        private readonly ProductContext productContext;
+    }
+
+}
