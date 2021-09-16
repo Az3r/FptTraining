99cf3d6dcd9321491784dc434bcda002f3776660
azer <azerprojects@gmail.com>
2021-09-16 08:08:30 +0700

    AspNet: product repository and unit of work

--- AspNet/Repositories/ProductRepository.cs
+++ AspNet/Repositories/ProductRepository.cs
@@ -5,44 +5,44 @@ using ProductServer.Models;
 
 namespace ProductServer.Repositories
 {
-    public class ProductRepository : IDisposable
+  public class ProductRepository : IDisposable
+  {
+    public ProductRepository(ProductContext context)
     {
-        public ProductRepository(ProductContext context)
-        {
-            this.context = context;
-        }
-
-        public List<Product> FindProduct(ProductQuery query, int size = 20, int offset = 0)
-        {
-            if (query.IsEmpty())
-            {
-                return context.Products.Skip(offset * size).Take(size).ToList();
-            }
-            List<Product> products = context.Products.Where(p => p.Name.Equals(query.Name, StringComparison.InvariantCultureIgnoreCase)).ToList();
-            return products;
-        }
-
-        public void Dispose()
-        {
-            context.Dispose();
-        }
-
-        private ProductContext context;
+      this.context = context;
     }
 
-    public class ProductQuery
+    public List<Product> FindProduct(ProductQuery query, int size = 20, int offset = 0)
     {
-        public string Name { get; set; } = "";
-        public string Category { get; set; } = "";
-        public double MinPrice { get; set; } = double.MinValue;
-        public double MaxPrice { get; set; } = double.MaxValue;
-
-        public bool IsEmpty()
-        {
-            return (MaxPrice == double.MaxValue &&
-                    MinPrice == double.MinValue &&
-                    String.IsNullOrWhiteSpace(Name) &&
-                    String.IsNullOrWhiteSpace(Category));
-        }
+      if (query.IsEmpty())
+      {
+        return context.Products.Skip(offset * size).Take(size).ToList();
+      }
+      List<Product> products = context.Products.Where(p => p.Name.Equals(query.Name, StringComparison.InvariantCultureIgnoreCase)).ToList();
+      return products;
     }
+
+    public void Dispose()
+    {
+      context.Dispose();
+    }
+
+    private ProductContext context;
+  }
+
+  public class ProductQuery
+  {
+    public string Name { get; set; } = "";
+    public string Category { get; set; } = "";
+    public double MinPrice { get; set; } = double.MinValue;
+    public double MaxPrice { get; set; } = double.MaxValue;
+
+    public bool IsEmpty()
+    {
+      return (MaxPrice == double.MaxValue &&
+              MinPrice == double.MinValue &&
+              String.IsNullOrWhiteSpace(Name) &&
+              String.IsNullOrWhiteSpace(Category));
+    }
+  }
 }
--- AspNet/Repositories/UnitOfWork.cs
+++ AspNet/Repositories/UnitOfWork.cs
@@ -3,28 +3,27 @@ using ProductServer.Models;
 
 namespace ProductServer.Repositories
 {
-    public class UnitOfWork : IDisposable
+  public class UnitOfWork : IDisposable
+  {
+    public UnitOfWork(ProductContext product)
     {
-        public UnitOfWork(ProductContext product)
-        {
-            productContext = product;
-            ProductRepository = new ProductRepository(product);
-
-        }
-
-        public void Dispose()
-        {
-            productContext.Dispose();
-        }
-
-        public void Save()
-        {
-            productContext.SaveChanges();
-        }
-
-        public readonly ProductRepository ProductRepository;
-
-        private readonly ProductContext productContext;
+      productContext = product;
+      ProductRepository = new ProductRepository(product);
     }
+
+    public void Dispose()
+    {
+      productContext.Dispose();
+    }
+
+    public void Save()
+    {
+      productContext.SaveChanges();
+    }
+
+    public readonly ProductRepository ProductRepository;
+
+    private readonly ProductContext productContext;
+  }
 
 }
